4c723c403772e2fe5dc1d7b4fc68102a889a741d
Luke566178 <lrhody2031@campbell.k12.mo.us>
2025-12-05 21:21:53 -0600

    rare-chickens.js

---
+++ rare-chickens.js
@@ -0,0 +1,5 @@
+// --- Rare Chickens ---
+
+const silverChicken = { name: "Silver Chicken", chance: 0.01, duration: 15, multiplier: 1.5 };
+const goldChicken   = { name: "Gold Chicken", chance: 0.003, duration: 60, multiplier: 2 };
+const diamondChicken= { name: "Diamond Chicken", chance: 0.0005, duration: 40, multiplier: 3.5 };
